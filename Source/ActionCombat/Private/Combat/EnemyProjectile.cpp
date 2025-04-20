1da8604f2612146b975f1f1cfb95964ed5df1ddb
Anthony Durén <anthonydadivasduren@gmail.com>
2025-04-20 17:52:21 +0200

    feat: Enhance enemy projectile hit behavior
    Added collision response to stop projectile movement on player hit
    and spawn hit VFX particles for better visual feedback.

--- Source/ActionCombat/Private/Combat/EnemyProjectile.cpp
+++ Source/ActionCombat/Private/Combat/EnemyProjectile.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Combat/EnemyProjectile.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
 AEnemyProjectile::AEnemyProjectile()
@@ -31,6 +33,12 @@ void AEnemyProjectile::HandleBeginOverlap(AActor* OtherActor)
 
 	if (!PawnRef->IsPlayerControlled()) { return; }
 
-	UE_LOG(LogTemp, Warning, TEXT("Overlapped With PLayer: %s"), *PawnRef->GetName());
+	//UE_LOG(LogTemp, Warning, TEXT("Overlapped With PLayer: %s"), *PawnRef->GetName());
+
+	FindComponentByClass<UParticleSystemComponent>()
+		->SetTemplate(HitTemplate);
+
+	FindComponentByClass<UProjectileMovementComponent>()
+		->StopMovementImmediately();
 }
 
